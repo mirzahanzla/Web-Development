6c5a556bd7d3e007ffb96faec21da270db7485b4
Hanzla <mirzamhanzla@gmail.com>
2023-07-30 18:57:58 +0500

    Initial commit

---
+++ PracticeProblem_JS/Weak_02_Task/variable.js
@@ -0,0 +1,17 @@
+let number1=20;
+let number2=30;
+let result=++number1;
+console.log(result);
+console.log(number2);
+
+const var1="Hanzla";
+const var2=(function(){
+    return "Hello, My Name is ";
+})();
+const var3 =var2+var1;
+console.log(var3);
+
+const arryinput=[10,'hanzla',{poor:'good', rich:12},[1,2]];
+console.log( arryinput[2].poor);
+const inputno={num:3,num2:5,num3:94};
+console.log(inputno.num);

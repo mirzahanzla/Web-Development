5f3278c59de0ea8d3605d4d500d99145bb2f7758
Hanzla <mirzamhanzla@gmail.com>
2023-08-08 22:44:19 +0500

    Practice javascript

--- Javacript Concept/OOP_Js/module/module.js
+++
@@ -1,8 +0,0 @@
-// const {hello, ahello} = require("./module1") // commonjs
-import hanzla, {hello, ahello} from "./module2.js";
-hello()
-ahello("Rizwan")
-ahello("Ali")
-ahello("Azim")
-ahello("Saif")
-hanzla()
